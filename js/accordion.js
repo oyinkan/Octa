5c6e5c39b5c1d135752b2c460ed5be0860be73fe
oyinkan <skusman@student.lautech.edu.ng>
2019-07-11 11:57:33 +0100

    made changes to the webpage

--- js/accordion.js
+++ js/accordion.js
@@ -1,18 +1,8 @@
-function goToTwo() {
-    var num2 = '#group-2';
-
-    document.querySelector(num2).scrollIntoView();
-
-    $(num2).trigger('click');
-}
-
 function toggleIcon(e) {
     $(e.target)
-    .prev('.panel-heading')
-    .find(".more-less")
-    $(".open").toggle()
-    $(".panel-title a").css('font-weight', '500');
+        .prev('.panel-heading')
+        .find(".more-less")
+        .toggleClass('fa fa-angle-right fa fa-angle-down');
 }
 $('.panel-group').on('hidden.bs.collapse', toggleIcon);
 $('.panel-group').on('shown.bs.collapse', toggleIcon);
-
